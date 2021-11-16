6a691b224b73cdbc4be070aeb7d798f7e3ac7854
“AMIR <amiraj1011@gmail.com>
2021-11-16 14:03:18 -0800

    amir for UC6 for testing password pattern

--- UserRegistrationTesting/UserRegistration.cs
+++ UserRegistrationTesting/UserRegistration.cs
@@ -91,7 +91,8 @@ namespace RegularExpression00
         {
             //Regular expression of password
             //Rule 1 : minimum 8 charactors
-            string password = "^[a-zA-Z]{8}$";
+            //Rule 2 : atleast 1 uppercase
+            string password = "^(?=.*[A-Z])[a-zA-Z0-9]{8,}$";
             Regex regex = new Regex(password);
 
             if (regex.IsMatch(name))
